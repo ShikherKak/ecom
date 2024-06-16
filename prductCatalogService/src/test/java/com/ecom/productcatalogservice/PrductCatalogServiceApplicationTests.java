96b0aa44ddd2fd794420a01c1b66d062c762cf45
yogeshsunder <shikherkak@gmail.com>
2024-06-17 01:50:54 +0530

    I have added the product catalog service and connected to my DB using Env Variables

---
+++ prductCatalogService/src/test/java/com/ecom/productcatalogservice/PrductCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.productcatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrductCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
