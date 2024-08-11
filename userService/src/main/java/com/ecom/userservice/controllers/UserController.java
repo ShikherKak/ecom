b6963c84295fc4994a4dacb8c6a9811a5a809f28
Shikher Kak <shikherkak@gmail.com>
2024-08-11 21:53:37 +0530

    learning spring security

--- userService/src/main/java/com/ecom/userservice/controllers/UserController.java
+++ userService/src/main/java/com/ecom/userservice/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.ecom.userservice.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping("/hi")
-    public void hello()
+    public ResponseEntity<String> hello()
     {
-        System.out.println("Hello");
+        return ResponseEntity.ok("Hello from the other side");
+
+
     }
 
 }
